52bbb440cb975d26523376ae4d251e4cc609c870
ArianeDahl <ariane.dahl@googlemail.com>
2023-02-14 14:58:16 +0100

    add update Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,7 +4,7 @@ export default function Form({ onAddActivity }) {
     const [activityName, setActivityName] = useState("")
     const [isGoodWeather, setIsGoodWeather] = useState(false)
 
-    function handleSubmit() {
+    function handleSubmit(event) {
         onAddActivity(activityName, isGoodWeather);
         setActivityName("");
         setIsGoodWeather(false);
@@ -16,12 +16,16 @@ return (
         <label>
             Name of activity:
             <input type="text"
-            value={activityName}/>
+            value={activityName}
+            onChange={(event) => setActivityName(event.target.value)}
+            />
         </label>
         <label>
             Is this a good weather activity?
-            <input type="checkbox"
-            checked={isGoodWeather}/>
+            <input type="checkbox" 
+            checked={isGoodWeather}
+            onChange={(event) => setIsGoodWeather(event.target.checked)}
+            />
         </label>
         <button type="submit">Submit</button>
     </form>
